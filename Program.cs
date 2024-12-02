d84c1c47de89c8f48f8b2967da4e6494a5a98a7a
Sunday Othniel <Sundayothniel@outlook.com>
2024-12-02 20:56:35 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Fibbonaccia
+{
+    static string Fibb(int length = 3)
+    {
+        List<int> values = new List<int> { 1, 1 };
+        for (int i = 2; i < length; i++)
+        {
+            int c = values[^1] + values[^2];
+            values.Add(c);
+        }
+        return $"[{string.Join(", ", values)}]";
+    }
+    static void Main()
+    {
+        Console.WriteLine(Fibb(20));
+    }
+}
